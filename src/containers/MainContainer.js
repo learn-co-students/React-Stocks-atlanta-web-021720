ce9b45af764c72062d0ccf480bd698d566cf4e5c
ChanPark <thechanmoon@gmail.com>
2020-04-26 15:22:09 -0400

    filter & sort

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -8,7 +8,8 @@ class MainContainer extends Component {
   state = {
     stocks: [],
     portfolios: [],
-    filter: false
+    filter: 'All',
+    sorts: []
   }
 
   componentDidMount()
@@ -34,16 +35,61 @@ class MainContainer extends Component {
     // console.log('sellStock index', index)
     this.setState({portfolios: this.state.portfolios.filter((item, idx) => idx !== index)})    
   }
- 
+  
+  cbSortHandler = (event) =>
+  {
+    //debugger
+    console.log('sh',event.target.checked)
+    console.log('sh',event.target.value)
+    // if(!this.state.sorts.find((sort)=>{sort===event.target.value}))
+    //   this.setState({sorts : [...this.state.sorts, event.target.value]})
+    if(this.state.sorts.indexOf(event.target.value)===-1)
+      this.setState({sorts : [...this.state.sorts, event.target.value]})
+  }
+
+  cbFilterHandler = (event) =>
+  {
+    console.log('fh',event.target)
+    this.setState({filter: event.target.value})
+  }
+
+  getStocks()
+  {
+    let retVal = [...this.state.stocks];
+    
+    if(this.state.filter !== 'All')
+    {
+      retVal = retVal.filter(
+        (stock)=>{return stock.type === this.state.filter}
+      )
+    }
+
+    
+    if(this.state.sorts.indexOf('Alphabetically')!==-1)
+    {
+      // debugger
+      retVal.sort((a,b)=>{return (a.name<b.name? -1:1)})
+    }
+
+    if(this.state.sorts.indexOf('Price')!==-1)
+    {
+      // debugger
+      retVal.sort((a,b)=>{return (a.price>b.price? -1:1)}) 
+    }
+
+    return retVal;
+  }
+
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar cbSortHandler={this.cbSortHandler} cbFilterHandler={this.cbFilterHandler}/>
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer {...this.state} buyStock={this.buyStock}/>
+              {/* <StockContainer {...this.state} buyStock={this.buyStock}/> */}
+              <StockContainer stocks={this.getStocks()} buyStock={this.buyStock}/>
 
             </div>
             <div className="col-4">
