7073f7fb3d463ff25216c5e948606b1108584f37
ChanPark <thechanmoon@gmail.com>
2020-04-26 00:43:21 -0400

    addtoPortfolio

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -5,6 +5,34 @@ import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
 
+  state = {
+    stocks: [],
+    portfolios: [],
+    filter: false
+  }
+
+  componentDidMount()
+  {
+    fetch('http://localhost:3000/stocks')
+    .then(res=>res.json())
+    .then(data=>{
+      this.setState({stocks: data})
+      }
+    )
+  }
+
+  buyStock = (stock) =>
+  {
+    //console.log('buyStock', stock)
+    this.setState({portfolios: [...this.state.portfolios, stock]})
+  }
+
+  sellStock = (stock) =>
+  {
+    //console.log('buyStock', stock)
+    this.setState({portfolios: [...this.state.portfolios, stock]})
+  }
+ 
   render() {
     return (
       <div>
@@ -13,12 +41,12 @@ class MainContainer extends Component {
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer {...this.state} buyStock={this.buyStock}/>
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer {...this.state} buyStock={this.sellStock}/>
 
             </div>
           </div>
--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -3,12 +3,23 @@ import Stock from '../components/Stock'
 
 class PortfolioContainer extends Component {
 
+  renderStocks()
+  {
+    // return <Stock stocks={this.props.stocks}/>
+    return ( this.props.portfolios.map( stock => {
+        return ( <Stock key = {stock.id} stock={stock} sellStock={this.props.sellStock}/> )
+        }
+      )
+    )
+  }
+
   render() {
     return (
       <div>
         <h2>My Portfolio</h2>
           {
             //render your portfolio stocks here
+            this.renderStocks()
           }
       </div>
     );
