2fb8bceafd96dc74699126f5f8d3e5abd6e8ff8f
gcampbell391 <gcampbell391@gmail.com>
2020-04-27 11:58:22 -0400

    Done.

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
-const Stock = () => (
+const Stock = (props) => (
   <div>
 
     <div className="card">
-      <div className="card-body">
+      <div className="card-body" onClick={props.handleClick}>
         <h5 className="card-title">{
-            //Company Name
-          }</h5>
+          props.stock.name
+        }</h5>
         <p className="card-text">{
-            //ticker: stock price
-          }</p>
+          props.stock.ticker
+        }</p>
       </div>
     </div>
 
--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -4,12 +4,15 @@ import Stock from '../components/Stock'
 class PortfolioContainer extends Component {
 
   render() {
+    const portfolioStocks = this.props.portfolioStocks
     return (
       <div>
         <h2>My Portfolio</h2>
-          {
-            //render your portfolio stocks here
-          }
+        {
+          portfolioStocks.map(stock => {
+            return <Stock stock={stock} key={`portfolio${stock.id}`} handleClick={() => this.props.handleClick("portfolio", stock)} />
+          })
+        }
       </div>
     );
   }
